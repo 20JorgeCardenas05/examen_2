9a365a2595cb0bd371149e475936d8f5fd6041a0
20JorgeCardenas05 <jcardenas74@ucol.mx>
2023-11-29 20:20:27 -0600

    Second commit

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
-const eventos = [ "desayuno", "comida", "cena", "fiesta", "reunion", "posada", "baile", "peda", "viaje", "pijamada" ];
+let eventos = [ "desayuno", "comida", "cena", "fiesta", "reunion", "posada", "baile", "peda", "viaje", "pijamada" ];
 
 showDom( "array1", eventos );
 
 const bot1 = document.getElementById("verde");
+const bot2 = document.getElementById("rojo");
 
-bot1.addEventListener( "click", () => {
-    let nuevo = document.getElementById( "nuevo" ).value;
-    eventos.push( nuevo );
+bot1.addEventListener("click", () => {
+    let nuevo = document.getElementById("nuevo").value;
+    eventos.push(nuevo);
     document.getElementById("textoo").reset();
-} );
+    showDom("array1", eventos); // Actualiza la vista del DOM
+});
 
+bot2.addEventListener("click", () => {
+    eventos.pop();
+    showDom("array1", eventos); // Actualiza la vista del DOM
+});
 
 function showDom(elemento, arr) {
+    let listaHTML = "";
     for (let i = 0; i < arr.length; i++) {
-        document.getElementById(elemento).innerHTML +=
-            `<li>${arr[i]}</li>`;
+        listaHTML += `<li>${arr[i]}</li>`;
     }
+    document.getElementById(elemento).innerHTML = listaHTML;
 }
-
-// document.getElementById( "lis" ).innerHTML = eventos;
